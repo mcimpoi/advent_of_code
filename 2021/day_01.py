ef9d79b6ffcb1b539eb74e2c7b3906aaa5746d1a
Mircea Cimpoi <mircea@>
2021-12-01 08:22:57 +0100

    adding day 1

---
+++ 2021/day_01.py
@@ -0,0 +1,34 @@
+import os
+
+INPUT_FILE_P1 = "data/day_01.txt"
+
+
+def solve_day1_p1(input_file: str) -> int:
+    with open(input_file, "r") as f:
+        depths = [int(x.strip()) for x in f.readlines()]
+
+    cnt = 0
+    for ii in range(1, len(depths)):
+        if (depths[ii] > depths[ii - 1]):
+            cnt += 1
+    return cnt
+
+
+def solve_day1_p2(input_file: str) -> int:
+    with open(input_file, "r") as f:
+        depths = [int(x.strip()) for x in f.readlines()]
+
+    cnt = 0
+
+    # sliding[ii] = sliding[ii - 1] - depths[ii - 1] + depths[ii + 2]
+    # sliding[ii] > sliding[ii - 1] ==> depths[ii + 2] > depths[ii - 1]
+    for ii in range(1, len(depths) - 2):
+        if depths[ii + 2] > depths[ii - 1]:
+            cnt += 1
+
+    return cnt
+
+
+if __name__ == "__main__":
+    print(solve_day1_p1(INPUT_FILE_P1))
+    print(solve_day1_p2(INPUT_FILE_P1))
