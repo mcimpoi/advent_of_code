017e39748b24f8d118efc3fe7aeb2b44b5c58345
Mircea Cimpoi <1849908+mcimpoi@users.noreply.github.com>
2023-05-01 15:50:24 +0200

    Aoc2022 (#3)
    * Add day 5 + sample inputs
    
    * Hacky version for Day6
    
    * Fix day 7 and 8
    
    * Add day 12 + formatting
    
    * Add day 13
    
    * Add day 14 inputs.
    
    ---------
    
    Co-authored-by: Mircea Cimpoi <mircea@suprafata>

---
+++ 2022/day07.py
@@ -0,0 +1,106 @@
+from collections import defaultdict
+from io import DEFAULT_BUFFER_SIZE
+from typing import List, Tuple, Union, Optional, Dict
+from collections import deque
+
+INPUT_FILE: str = "2022/data/day_07.txt"
+MAX_DIR_SIZE: int = 100000
+
+class File:
+    def __init__(self, name: str, size: int) -> None:
+        self.name = name
+        self.size = size
+
+class Directory:
+    def __init__(self, name: str, parent: Optional["Directory"]) -> None:
+        self.total_size: int = 0
+        self.subdirs: Dict[str, Directory] = {}
+        self.files: List[File] = []
+        self.parent: Optional[Directory] = parent
+        self.updated = False
+
+def parse_input(input_file: str) -> Directory:
+    with open(input_file, "r") as f:
+        lines = [x.strip() for x in f.readlines()]
+    root = Directory("<ROOT>", None)
+    root.subdirs["/"] = Directory("/", None)
+    idx = 0
+    crt_path = []
+    crt_dir = root
+
+    while idx < len(lines):
+        if lines[idx].startswith("$ cd"):
+            dir_name = lines[idx][5:]
+            if dir_name == "..":
+                # crt_path.pop()
+                crt_dir = crt_dir.parent if crt_dir is not None else root
+            else:
+                if crt_dir is None:
+                    raise ValueError(f"This should not happen: Line: {lines[idx]}")
+                crt_dir = crt_dir.subdirs[dir_name]
+
+            idx += 1
+        elif lines[idx].startswith("$ ls"):
+            idx += 1
+            if crt_dir is None:
+                raise ValueError("This should not happen")
+            while idx < len(lines) and not lines[idx].startswith("$"):
+                if lines[idx].startswith("dir"):
+                    subdir_name = lines[idx].split()[1]
+                    crt_dir.subdirs[subdir_name] = Directory(subdir_name, crt_dir)
+                else:
+                    parts = lines[idx].split()
+                    filename = parts[1]
+                    filesize = int(parts[0])
+                    crt_dir.files.append(File(filename, filesize))
+                idx += 1
+    return root
+
+def update_total_size(root: Directory) -> Directory:
+    if root.updated:
+        return root
+    total_size = sum(x.size for x in root.files)
+    for subdir in root.subdirs:
+        update_total_size(root.subdirs[subdir])
+        total_size += root.subdirs[subdir].total_size
+    root.total_size = total_size
+    return root
+
+def day_05_part1(input_file: str) -> int:
+    root = parse_input(input_file)
+    update_total_size(root.subdirs["/"])
+    
+    q = deque()
+    q.append(root.subdirs["/"])
+    total_sum = 0 
+    while len(q) > 0:
+        crt = q.pop()
+        if crt.total_size <= MAX_DIR_SIZE:
+            total_sum += crt.total_size
+        for subdir in crt.subdirs:
+            q.append(crt.subdirs[subdir]) 
+
+    return total_sum
+
+def day_05_part2(input_file: str) -> int:
+    SYSTEM_SIZE = 70000000
+    NEEDED_FREE = 30000000
+    root = parse_input(input_file)
+    update_total_size(root.subdirs["/"])
+    to_delete = NEEDED_FREE - (SYSTEM_SIZE - root.subdirs["/"].total_size)
+
+    q = deque( )   
+    q.append(root.subdirs["/"])
+    smallest_size = SYSTEM_SIZE
+    while len(q) > 0:
+        crt = q.pop()
+        if crt.total_size >= to_delete:
+            smallest_size = min(smallest_size, crt.total_size)
+        for subdir in crt.subdirs:
+            q.append(crt.subdirs[subdir]) 
+
+    return smallest_size
+    
+if __name__ == "__main__": 
+    print(day_05_part1(INPUT_FILE))
+    print(day_05_part2(INPUT_FILE))
---
+++ 2022/day13.py
@@ -0,0 +1,85 @@
+from collections import defaultdict
+from io import DEFAULT_BUFFER_SIZE
+from typing import Any, List, Tuple, Union, Optional, Dict
+from collections import deque
+
+INPUT_FILE: str = "2022/data/day_13.txt"
+
+
+def parse_inputs(input_file: str) -> List[Any]:
+    with open(input_file, "r") as f:
+        data = [x.strip() for x in f.readlines() if len(x.strip()) > 0]
+
+    return [eval(x) for x in data]
+
+
+def day_13_part1(input_file: str) -> int:
+    pairs = parse_inputs(input_file)
+
+    # print(in_order(eval("[7, 7]"), eval("[7]")))
+
+    total = 0
+    for ii in range(len(pairs) // 2):
+        left = pairs[2 * ii]
+        right = pairs[2 * ii + 1]
+        res = in_order(left, right)
+        if res:
+            total += ii + 1
+
+    return total
+
+
+def in_order(left: List, right: List) -> bool:
+    if left is None and right is None:
+        return True
+    if left is None and right is not None:
+        return True
+    if len(left) == 0 and len(right) > 0:
+        return True
+    if len(left) > 0 and len(right) == 0:
+        return False
+    if isinstance(left[0], int) and isinstance(right[0], int):
+        if left[0] < right[0]:
+            return True
+        elif left[0] > right[0]:
+            return False
+        else:
+            return in_order(left[1:], right[1:])
+    if isinstance(left[0], list) and isinstance(right[0], list):
+        if left[0] == right[0]:
+            return in_order(left[1:], right[1:])
+        else:
+            return in_order(left[0], right[0])
+    if isinstance(left[0], int) and isinstance(right[0], list):
+        if [left[0]] == right[0]:
+            return in_order(left[1:], right[1:])
+        else:
+            return in_order([left[0]], right[0])
+    if isinstance(left[0], list) and isinstance(right[0], int):
+        if left[0] == [right[0]]:
+            return in_order(left[1:], right[1:])
+        else:
+            return in_order(left[0], [right[0]])
+    return in_order(left[1:], right[1:])
+
+
+def day_13_part2(input_file: str) -> Tuple[int, int, int]:
+    pairs = parse_inputs(input_file)
+
+    # print(in_order(eval("[7, 7]"), eval("[7]")))
+
+    lt2 = 1
+    lt6 = 2
+    for item in pairs:
+        res = in_order(item, [[2]])
+        if res:
+            lt2 += 1
+        if in_order(item, [[6]]):
+            lt6 += 1
+
+    return (lt2, lt6, lt2 * lt6)
+
+
+if __name__ == "__main__":
+    print(day_13_part1(INPUT_FILE))
+    print(day_13_part2(INPUT_FILE))
