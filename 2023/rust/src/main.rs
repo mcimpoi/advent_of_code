165bafb86d1f3e84cbb17c43a68592c0a42430f3
Mircea C <1849908+mcimpoi@users.noreply.github.com>
2023-12-01 10:09:17 +0100

    Adding python and rust implementations for day 1

---
+++ 2023/rust/src/main.rs
@@ -0,0 +1,7 @@
+mod day_01;
+use day_01::{solve_day_01_part_1, solve_day_01_part_2};
+
+fn main() {
+    println!("Result: {}", solve_day_01_part_1("data/day_01.txt"));
+    println!("Result: {}", solve_day_01_part_2("data/day_01.txt"));
+}
