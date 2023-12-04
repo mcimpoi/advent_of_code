240768f7bd154887e166ed1c6e12baf61bd363ea
Mircea C <1849908+mcimpoi@users.noreply.github.com>
2023-12-04 08:56:52 +0100

    Adding day 2 and day 3

--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,7 +1,21 @@
-mod day_01;
-use day_01::{solve_day_01_part_1, solve_day_01_part_2};
+// Runner for Advent of Code.
+// import module day_XX and
+// call solve_day_XX_part_1 and solve_day_XX_part_2
+
+// Assumes inputs are in data/day_XX.txt
+mod day_03;
+
+use day_03::{solve_day_03_part_1, solve_day_03_part_2};
 
 fn main() {
-    println!("Result: {}", solve_day_01_part_1("data/day_01.txt"));
-    println!("Result: {}", solve_day_01_part_2("data/day_01.txt"));
+    println!(
+        "Result (small): {}",
+        solve_day_03_part_1("data/day_03_small.txt")
+    );
+    println!(
+        "Result (small): {}",
+        solve_day_03_part_2("data/day_03_small.txt")
+    );
+    println!("Result: {}", solve_day_03_part_1("data/day_03.txt"));
+    println!("Result: {}", solve_day_03_part_2("data/day_03.txt"));
 }
